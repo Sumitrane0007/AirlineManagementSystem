58a290b2d2a17adcb3ce4d58c7dfe80eb590ee98
sumit <sumit@LAPTOP-DTO6AGC1>
2024-08-13 23:42:40 +0530

    new project added

---
+++ src/airlinemanagementsystem/Conn.java
@@ -0,0 +1,22 @@
+     
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem","root","9421");
+            s = c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
